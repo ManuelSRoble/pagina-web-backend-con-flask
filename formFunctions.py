3b392ff8452b19612c98dd11d0fc22d511aa2beb
Manuel Suarez <roblecito2023@gmail.com>
2024-05-03 13:50:24 -0300

    agregue pagina tres en raya html, css y js; y pagina admin para ver datos de users

--- formFunctions.py
+++ formFunctions.py
@@ -7,10 +7,6 @@ from flask_wtf import CSRFProtect
 def validar_usuario(form, field):
   if not field.data.isalpha():
     raise ValidationError('El usuario solo debe contener letras')
-  
-def validar_contrasena(form, field):
-  if len(field.data) < 4:
-    raise ValidationError('La contra debe contener 4 caracteres como minimo')
 
 def validar_confirmar_contrasena(form, field):
   if field.data != form.contrasena.data:
@@ -18,7 +14,7 @@ def validar_confirmar_contrasena(form, field):
   
 def validar_edad(form, field):
   if not 18 <= field.data <=120:
-    raise ValidationError('La edad esta fuera del rango del 18 al 120')
+    return ValidationError('La edad esta fuera del rango del 18 al 120')
   
 def validar_correo(form, field):
   #lista de dominios permitidos
@@ -31,7 +27,13 @@ class RegistroFormulario(FlaskForm):
   usuario = StringField('Usuario', validators=[ DataRequired(), Length(min=4, max=25), validar_usuario])
   
   correo = StringField('Email', validators=[DataRequired(), Email(), validar_correo])
-  contrasena = PasswordField('Password', validators=[DataRequired(), validar_contrasena])
-  confirmar_contrasena = PasswordField('Confirmar password', validators=[DataRequired(), validar_confirmar_contrasena])
+  
+  contrasena = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=10, message='Debe tener entre 4 y 10 caracteres')])
+  
+  confirmar_contrasena = PasswordField('Confirmar password', validators=[
+    DataRequired(), 
+    Length(min=4, max=10, message='Debe tener entre 4 y 10 caracteres'), 
+    validar_confirmar_contrasena])
+  
   edad = IntegerField('Edad/Age', validators=[DataRequired(), validar_edad])
   enviar = SubmitField('Enviar')
--- main.py
+++ main.py
@@ -8,19 +8,23 @@ app.config['SECRET_KEY'] = 'This is the most secure key to protect the forms'
 
 @app.route('/', methods=['POST', 'GET'])
 def registro():
-  if request.method == 'POST':
-    nombreDeUsuario = request.form['usuario']
-    email = request.form['correo']
-    clave = request.form['contrasena']
-    edad = request.form['edad']
-    
-    '''
-    datos = mostrar_usuarios()
-    #renderizar la plantilla html y pasar los datos recuperados como contexto
-    return render_template('tres_en_raya.html', data = datos) 
-    '''
-    
   form = RegistroFormulario()
+  if form.validate_on_submit():
+    if request.method == 'POST':
+      nombreDeUsuario = request.form['usuario']
+      email = request.form['correo']
+      clave = request.form['contrasena']
+      # confirmacionDeClave = request.form['confirmar_contrasena']
+      edad = request.form['edad']
+      #if clave == confirmacionDeClave:
+      agregar_usuario(nombreDeUsuario, email, clave, edad)
+      return '<h1>Usuario agregado</h1>'
+    
+      '''
+      datos = mostrar_usuarios()
+      #renderizar la plantilla html y pasar los datos recuperados como contexto
+      return render_template('tres_en_raya.html', data = datos) 
+      '''
   # if form.validate_on_submit():
   #   return 'Formulario validado con exito'
   return render_template('formularioDeRegistro.html', form = form)
@@ -28,6 +32,13 @@ def registro():
 @app.route('/login')
 def login():
   pass
+
+@app.route('/admin')
+def admin():
+  #ver datos de la tabla usuario de bd
+  datos = mostrar_usuarios()
+  titulo = 'Ventana Admin'
+  return render_template('admin.html', datos=datos, titulo = titulo)
 
 if __name__ == '__main__':
   app.run(debug=True)
